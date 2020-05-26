85d456b4eca2791e63f0b607f0d397d5f4a0c20c
Tozzigh <tozzigh@gmail.com>
2020-05-26 10:06:47 +0000

    If statments, past and future

--- src/js/component/InputToDo.js
+++ src/js/component/InputToDo.js
@@ -1,30 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function InputToDo() {
 	const [taskList, setTaskList] = useState([]);
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/tozzigh")
+			.then(response => response.json())
+			.then(data => {
+				for (let z in data) {
+					setTaskList(taskList => [...taskList, data[z]]);
+				}
+			});
+	}, []);
+
+	useEffect(
+		() => {
+			fetch("https://assets.breatheco.de/apis/fake/todos/user/tozzigh", {
+				method: "PUT",
+				headers: { "content-type": "application/json" },
+				body: JSON.stringify(taskList)
+			});
+		},
+		[taskList]
+	);
 
 	return (
 		<div className="main">
 			<h1>todos</h1>
 			<input type="text" placeholder="What need to be done?" onKeyPress={handleTaskListChange} />
-			<ul>
-				{taskList.map((todo, index) => (
-					<li className="todos" key={index}>
-						{todo}
-						<i className="delLi fa fa-times" onClick={() => onDeleteClicked(index)} />
-					</li>
-				))}
-				{taskCounter()}
-			</ul>
+			{createLi(taskList)}
 		</div>
 	);
+
+	function createLi(list) {
+		const listaa = list.map((elemento, index) => {
+			if (elemento.done === true) {
+				return (
+					<li className="todos" key={index}>
+						{elemento.label}
+						<i className="delLi fa fa-times" onClick={() => onDeleteClicked(index)} />
+					</li>
+				);
+			}
+		});
+		return (
+			<ul>
+				{listaa}
+				{taskCounter()}
+			</ul>
+		);
+	}
 
 	function onDeleteClicked(index) {
 		setTaskList(taskList.filter((item, pos) => pos !== index));
 	}
 
 	function taskCounter() {
-		const leng = taskList.length;
+		const leng = taskList.length - 1;
 		if (leng === 0) {
 			return <li className="taskCounter text-muted">No tasks, add a task</li>;
 		} else if (leng === 1) {
@@ -37,7 +69,7 @@ export function InputToDo() {
 	function handleTaskListChange(e) {
 		if (e.key === "Enter") {
 			if (e.target.value.split(" ").join("").length > 0) {
-				setTaskList([...taskList, e.target.value]);
+				setTaskList([...taskList, { label: e.target.value, done: true }]);
 			}
 			e.target.value = "";
 		}
